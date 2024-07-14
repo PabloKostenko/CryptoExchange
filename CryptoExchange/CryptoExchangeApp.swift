de68970e6df4ef24010e68fb8e7d47125fce69e4
pablokostenko <pshkostenko@gmail.com>
2024-07-14 12:37:02 +0200

    Initial Commit

---
+++ CryptoExchange/CryptoExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoExchangeApp.swift
+//  CryptoExchange
+//
+//  Created by Pavlo Kostenko on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
